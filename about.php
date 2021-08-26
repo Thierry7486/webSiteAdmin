aa67bf3ac31844d862af53a487d79ab4bb33ac8a
Thierry7486 <thierrymalherbe@live.fr>
2021-08-26 10:28:58 +0200

    ajout d'une page about ainsi que son administration

---
+++ about.php
@@ -0,0 +1,33 @@
+<?php
+require("./inc/library.php")
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <?php include("./parts/head.php") ?>
+    <title>About - Hello PHP !</title>
+</head>
+<body>
+    <!-- NavBar -->
+    <?php include("./parts/navbar.php") ?>
+    <!-- End NavBar -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <h1>Home, Hello PHP !</h1>
+            </div>
+        </div>
+        <div class="row">
+            
+            <div class="col-12 text-center">
+                <p>
+                    <?=nl2br(readAllTxt("./assets/about/content.txt"));?>
+                </p>
+            </div>
+        </div>
+    </div>
+    <!-- Footer -->
+    <?php include("./parts/footer.php") ?>
+    <!-- End Footer -->
+</body>
+</html>
